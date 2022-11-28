bcbe184beffb10e40a22861cc4ce6decb0246933
amusingaxl <112016538+amusingaxl@users.noreply.github.com>
2022-11-28 11:23:42 -0300

    refactor: allow additional args on deploy script

--- src/RWA007-A/scripts/deploy.sh
+++ src/RWA007-A/scripts/deploy.sh
@@ -8,6 +8,9 @@ set -eo pipefail
 source "${BASH_SOURCE%/*}/../../../scripts/_common.sh"
 
 NETWORK=$1
+shift
+ARGS="$@"
+
 [[ "$NETWORK" && ("$NETWORK" == "mainnet" || "$NETWORK" == "goerli" || "$NETWORK" == "ces-goerli") ]] || die "Please set NETWORK to one of ('mainnet', 'goerli', 'ces-goerli')"
 
 check-network $NETWORK
@@ -35,13 +38,13 @@ FORGE_SCRIPT="${BASH_SOURCE%/*}/../../../scripts/forge-script.sh"
 
 # estimate
 [ "$ESTIMATE" = "true" ] && {
-	RESPONSE=$($FORGE_SCRIPT "${BASH_SOURCE%/*}/RWA007Deployment.s.sol:RWA007Deployment" | tee >(cat 1>&2))
-	jq -R 'fromjson? | .logs | .[] | fromjson?' <<<"$RESPONSE" | jq -s 'map( {(.[0]): .[1]} ) | add'
-	exit 0
+    RESPONSE=$($FORGE_SCRIPT "${BASH_SOURCE%/*}/RWA007Deployment.s.sol:RWA007Deployment" $ARGS tee >(cat 1>&2))
+    jq -R 'fromjson? | .logs | .[]' <<<"$RESPONSE" | xargs -I@ cast --to-ascii @ | jq -R 'fromjson?' | jq -s 'map( {(.[0]): .[1]} ) | add'
+    exit 0
 }
 
-RESPONSE=$($FORGE_SCRIPT "${BASH_SOURCE%/*}/RWA007Deployment.s.sol:RWA007Deployment" --broadcast --slow --verify --retries 10 | tee >(cat 1>&2))
-jq -R 'fromjson? | .logs | .[] | fromjson?' <<<"$RESPONSE" | jq -s 'map( {(.[0]): .[1]} ) | add'
+RESPONSE=$($FORGE_SCRIPT "${BASH_SOURCE%/*}/RWA007Deployment.s.sol:RWA007Deployment" --broadcast --slow --verify --retries 10 $ARGS | tee >(cat 1>&2))
+jq -R 'fromjson? | .logs | .[]' <<<"$RESPONSE" | xargs -I@ cast --to-ascii @ | jq -R 'fromjson?' | jq -s 'map( {(.[0]): .[1]} ) | add'
 
 # IF we hit block limit we need to parse output from previouse command and set deployed addresses to the ENV (hopefully Foundry will fix vm.setEnv wich we can use for that)
 # Then we can simply run another method of Deployment contract and pick up this ENV vars there using `--sig "secondPartOfDeployment()"` flag of `forge script` command
